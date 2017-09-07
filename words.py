bfec5573e42891c9a9644289c73e2b4ad1c80e02
Joey Smith <joeysmith@gmail.com>
2017-09-07 15:30:01 -0600

    New definition of choose - takes one iterable argument (min, max)

--- words.py
+++ words.py
@@ -5,3 +5,12 @@
 import subprocess
 from sys import exit
 import random
+
+def choose(difficulty):
+    (min, max) = difficulty
+    cmd = "/usr/bin/grep -E '^.{{{},{}}}$' /usr/share/dict/words".format(min, max)
+    obj = subprocess.run(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE)
+    result = obj.stdout.decode('utf-8').strip().split("\n")
+    return random.choice(result)
