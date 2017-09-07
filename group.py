857556b3b0e3cc09cb8213dd269bd07cbf179516
Joey Smith <joeysmith@gmail.com>
2017-09-07 13:40:59 -0600

    Flake8 compliance

--- group.py
+++ group.py
@@ -1,11 +1,12 @@
 import words
 import ui
+
 import random
 import sys
 
 difficulty_levels = {
     "easy": {
-        'word length': (3,8),
+        'word length': (3, 8),
         'commonality': 1.0,
         'guesses': 15
     },
@@ -15,9 +16,9 @@ difficulty_levels = {
         'guesses': 9
     },
     "hard": {
-        'word length': (15,20),
+        'word length': (15, 20),
         'commonality': 1.0,
-        'guesses': random.randint(4,6)
+        'guesses': random.randint(4, 6)
     }
 }
 
@@ -29,20 +30,24 @@ def main():
     bank = []
 
     user = input("What is your name? ")
-    difficulty = input("How hard of a game would you like? {} ".format(list(difficulty_levels.keys())))
+    difficulty = input("How hard of a game would you like? {} ".
+                       format(list(difficulty_levels.keys())))
 
     while difficulty not in difficulty_levels.keys():
-        difficulty = input("How hard of a game would you like? {} ".format(list(difficulty_levels.keys())))
+        difficulty = input("How hard of a game would you like? {} ".
+                           format(list(difficulty_levels.keys())))
 
     our_word = 'mississippi'
 
     found = ['i', 's', 'p']
-    bank  = ['e', 'a', 't', 'o', 'h']
+    bank = ['e', 'a', 't', 'o', 'h']
 
-    # our_word = words.choose(length=difficulty_levels[difficulty]['word length'])
+    # our_word = words.choose(length=
+    # difficulty_levels[difficulty]['word length'])
     # ui.render(object='gallows', parts=0)
     # ui.render(object='game_state', word=our_word, found=found)
     # ui.render(object='bank', letters=bank)
+
 
 def game_loop(word, found, bank, guess_count, parts):
     while True:
@@ -51,7 +56,7 @@ def game_loop(word, found, bank, guess_count, parts):
             sys.exit()
         else:
             ui.render(object='gallows', parts=0)
-            ui.render(object='game_state', word=our_word, found=found)
+            ui.render(object='game_state', word=word, found=found)
             ui.render(object='bank', letters=bank)
 
 
@@ -59,7 +64,6 @@ def game_won(word, found):
     for letter in word:
         if letter not in found:
             return False
-
 
 
 if __name__ == "__main__":
